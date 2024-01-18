416f42ce3282678882f79263d753d2fdf01df598
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-01-18 10:53:40 +0100

    Initialize notebooks collection

--- RubBotApi/Models/NoteBooks.cs
+++ RubBotApi/Models/NoteBooks.cs
@@ -2,6 +2,10 @@ namespace RubBotApi.Models;
 
 public class NoteBooks
 {
+    public NoteBooks()
+    {
+        Notes = new HashSet<Note>();
+    }
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Name { get; set; }
     public ICollection<Note> Notes { get; set; }
