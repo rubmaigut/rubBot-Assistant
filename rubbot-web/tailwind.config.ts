348e5bb70779a0b80f1f74b530be47f874b43f52
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-01-18 14:57:55 +0100

    update tailwind setup

--- rubbot-web/tailwind.config.ts
+++ rubbot-web/tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary-bg': '#F1F1F1',
+        'secondary-bg': '#FFF3DA',
+        'primary-font': '#545454',
+        'primary-bright': '#FAAB4E',
+        'bot-message-bg': '#66564A'
+      }
     },
   },
   plugins: [],
